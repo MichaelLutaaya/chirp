fb7ae687072b73a056768a5e5b2890bc00e48404
Michael Lutaaya <michael@lutaaya.com>
2014-08-10 14:47:29 -0400

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,17 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to Chirp!"
       redirect_to @user
     else
       render 'new'
     end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_url
   end
 
   private
